e130fb37cba5b96644e987aadc1d65e5214cf0c8
gisu <su2333066@naver.com>
2022-04-04 00:29:34 +0900

    react start!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
-import Detail from "./routes/Detail"
-
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
         <Route path="/movie/:id">
           <Detail />
         </Route>
